6a72b80194f9d1e81822217ab214c213728f1f32
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-11-04 13:15:02 -0500

    lets tryu dflsjftjiokd'fsjk'glm'esaf

--- modules/provisioning.nix
+++ modules/provisioning.nix
@@ -28,31 +28,38 @@ in
             };
             phase1Identity = {
               type = types.str;
+              default = "";
               example = "eduroamIDENTITY";
             };
             username = mkOption {
               type = types.str;
+              default = "";
               example = "cnolan123";
             };
             serverDomainMask = mkOption {
               type = types.str;
+              default = "";
               example = "radius.node";
             };
             domain = mkOption {
               type = types.str;
+              default = "";
               example = "university.edu";
             };
             password = mkOption {
               type = types.str;
+              default = "";
               example = "password123";
             };
             passwordHash = mkOption {
               type = types.str;
+              default = "";
               example = "password123";
               description = "Preferred over `eduroam.password`. The hash can be generated via `printf '%s' 'REPLACE_WITH_YOUR_PASSWORD' | iconv -t utf16le | openssl md4 -provider legacy | cut -d' ' -f2`";
             };
             caCert = mkOption {
               type = types.path;
+              default = "";
               example = "/var/lib/iwd/ca.pem";
               description = "(optional) path to your school's eduroam CA certificate";
             };
@@ -80,13 +87,13 @@ in
           eduroamProvisioningFile = pkgs.writeText "${eduroamFile}" ''
             [Security]
             EAP-Method=PEAP
-            ${lib.optionalString (p1ID != null) "EAP-Identity=${p1ID}@${domain}"}
-            ${lib.optionalString (caCert != null) "EAP-PEAP-CACert=${caCert}"}
-            ${lib.optionalString (domainMask != null) "EAP-PEAP-ServerDomainMask=${domainMask}.${domain}"}
+            ${lib.optionalString (p1ID != "") "EAP-Identity=${p1ID}@${domain}"}
+            ${lib.optionalString (caCert != "") "EAP-PEAP-CACert=${caCert}"}
+            ${lib.optionalString (domainMask != "") "EAP-PEAP-ServerDomainMask=${domainMask}.${domain}"}
             EAP-PEAP-Phase2-Method=MSCHAPV2
             EAP-PEAP-Phase2-Identity=${uname}@${edu.domain}
-            ${lib.optionalString (psswd != null) "EAP-PEAP-Phase2-Password=${psswd}"}
-            ${lib.optionalString (hash != null) "EAP-PEAP-Phase2-Password-Hash=${hash}"}
+            ${lib.optionalString (psswd != "") "EAP-PEAP-Phase2-Password=${psswd}"}
+            ${lib.optionalString (hash != "") "EAP-PEAP-Phase2-Password-Hash=${hash}"}
 
             [Settings]
             Autoconnect=true
