6faf2799c1a01784b4e18356f814cbc52459748a
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-11-07 22:21:33 -0500

    update how file gets written

--- modules/provisioning.nix
+++ modules/provisioning.nix
@@ -54,6 +54,12 @@ in
               example = "password123";
               description = "Preferred over `eduroam.password`. The hash can be generated via `printf '%s' 'REPLACE_WITH_YOUR_PASSWORD' | iconv -t utf16le | openssl md4 -provider legacy | cut -d' ' -f2`";
             };
+            passwordPath = mkOption {
+              type = types.nullOr types.path;
+              default = null;
+              example = "/run/secrets/password";
+              description = "Path of a secret file containing your password";
+            };
             caCert = mkOption {
               type = types.nullOr types.path;
               default = null;
@@ -94,6 +100,7 @@ in
             ) "EAP-PEAP-ServerDomainMask=${edu.serverDomainMask}.${edu.domain}"}
             EAP-PEAP-Phase2-Method=MSCHAPV2
             EAP-PEAP-Phase2-Identity=${edu.username}@${edu.domain}
+            
             ${lib.optionalString (!builtins.isNull edu.password) "EAP-PEAP-Phase2-Password=${edu.password}"}
             ${lib.optionalString (
               !builtins.isNull edu.passwordHash
@@ -122,7 +129,29 @@ in
               ExecStart = ''
                 /run/current-system/sw/bin/bash -c ' \
                   mkdir -p ${iwdConfigDir} && \
-                  ln -sf ${eduroamProvisioningFile} ${iwdConfigDir}/${eduroamFileName} \
+                  SECRET = $(cat ${passwordPath}) \
+                  cat > ${iwdConfigDir}/${eduroamFileName}<< EOF
+                  [Security]
+                  EAP-Method=PEAP
+                  ${lib.optionalString (
+                    edu.phase1Identity != null
+                  ) "EAP-Identity=${edu.phase1Identity}@${edu.domain}"}
+                  ${lib.optionalString (edu.caCert != null) "EAP-PEAP-CACert=${edu.caCert}"}
+                  ${lib.optionalString (
+                    edu.serverDomainMask != null
+                  ) "EAP-PEAP-ServerDomainMask=${edu.serverDomainMask}.${edu.domain}"}
+                  EAP-PEAP-Phase2-Method=MSCHAPV2
+                  EAP-PEAP-Phase2-Identity=${edu.username}@${edu.domain}
+                  ${lib.optionalString (!builtins.isNull edu.password) "EAP-PEAP-Phase2-Password=${edu.password}"}
+                  ${lib.optionalString (!builtins.isNull edu.passwordpath) "EAP-PEAP-Phase2-Password=$SECRET"}
+                  ${lib.optionalString (
+                    !builtins.isNull edu.passwordHash
+                  ) "EAP-PEAP-Phase2-Password-Hash=${edu.passwordHash}"}
+
+                  [Settings]
+                  Autoconnect=true
+                  EOF
+                  
                 '
               '';
 
